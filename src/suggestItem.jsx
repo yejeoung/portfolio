dce02b69e3d015f4c6ab978a5799f95162f651f9
yejeoung <cyg99080312@gmail.com>
2025-01-14 21:33:25 +0900

    suggestItem 반응형 진행

--- src/suggestItem.jsx
+++ src/suggestItem.jsx
@@ -1,187 +1,168 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
 import { FaStar } from "react-icons/fa";
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import styles from "./suggestItem.module.css";
 
 // 작성 날짜 : 2025-01-13
 // 파일의 역할 : 추천 제품 item
 
 // yarn add react-slick
+// yarn add slick-carousel
 
 function SuggestItem() {
-    const [display, setDisplay] = useState(true);
-    const [width, setWidth] = useState(1280);
-
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 3,
+        // prevArrow: $('#prevArrow'), //이전 화살표만 변경
+        // nextArrow: $('#nextArrow'), //다음 화살표만 변경
     };
     return (
         <div className="slider-container">
-            <h2> Resizable Collapsible </h2>
-            <button className="button" onClick={() => setWidth(width + 100)}>
-                {" "}
-                increase{" "}
-            </button>
-            <button className="button" onClick={() => setWidth(width - 100)}>
-                {" "}
-                decrease{" "}
-            </button>
-            <button className="button" onClick={() => setDisplay(!display)}>
-                {" "}
-                toggle{" "}
-            </button>
-            <div
-                style={{
-                    width: width + "px",
-                    display: display ? "block" : "none"
-                }}
-            >
-                <Slider {...settings}>
-                    <div>
-                        <div className={styles.suggestItem}>
-                            <img src="/images/suggestImg01.png" alt="" />
-                            <div className={styles.sText}>
-                                <div className={styles.sText1}>
-                                    <span><span className={styles.bold}>BEST</span>콜라겐 그린티 세라마이드 탄력장벽크림 50mL + 리필</span>
-                                </div>
-                                <div className={styles.sText2}>
-                                    <p className={styles.sT1}>43,550</p>
-                                    <p className={styles.sT2}>35%</p>
-                                    <p className={styles.sT3}>67,000</p>
-                                </div>
-                                <div className={styles.hash}>
-                                    <span className={styles.hashT}>#탄력장벽</span>
-                                    <span className={styles.hashT}>#탄력장벽끌올</span>
-                                </div>
-                                <div className={styles.star}>
-                                    <span className={styles.starIcon}><FaStar /></span>
-                                    <span className={styles.score}>4.9 (18,012)</span>
-                                </div>
+            <Slider {...settings}>
+                <div>
+                    <div className={styles.suggestItem}>
+                        <img src="/images/suggestImg01.png" alt="" />
+                        <div className={styles.sText}>
+                            <div className={styles.sText1}>
+                                <span><span className={styles.bold}>BEST</span>콜라겐 그린티 세라마이드 탄력장벽크림 50mL + 리필</span>
+                            </div>
+                            <div className={styles.sText2}>
+                                <p className={styles.sT1}>43,550</p>
+                                <p className={styles.sT2}>35%</p>
+                                <p className={styles.sT3}>67,000</p>
+                            </div>
+                            <div className={styles.hash}>
+                                <span className={styles.hashT}>#탄력장벽</span>
+                                <span className={styles.hashT}>#탄력장벽끌올</span>
+                            </div>
+                            <div className={styles.star}>
+                                <span className={styles.starIcon}><FaStar /></span>
+                                <span className={styles.score}>4.9 (18,012)</span>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <div className={styles.suggestItem}>
-                            <img src="/images/suggestImg02.png" alt="" />
-                            <div className={styles.sText}>
-                                <div className={styles.sText1}>
-                                    <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 앰플</span>
-                                </div>
-                                <div className={styles.sText2}>
-                                    <p className={styles.sT1}>28,000</p>
-                                    <p className={styles.sT2}>30%</p>
-                                    <p className={styles.sT3}>40,000</p>
-                                </div>
-                                <div className={styles.hash}>
-                                    <span className={styles.hashT}>#항산화안티에이징앰플</span>
-                                    <span className={styles.hashT}>#안티에이징앰플</span>
-                                </div>
-                                <div className={styles.star}>
-                                    <span className={styles.starIcon}><FaStar /></span>
-                                    <span className={styles.score}>4.9 (56,096)</span>
-                                </div>
+                </div>
+                <div>
+                    <div className={styles.suggestItem}>
+                        <img src="/images/suggestImg02.png" alt="" />
+                        <div className={styles.sText}>
+                            <div className={styles.sText1}>
+                                <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 앰플</span>
+                            </div>
+                            <div className={styles.sText2}>
+                                <p className={styles.sT1}>28,000</p>
+                                <p className={styles.sT2}>30%</p>
+                                <p className={styles.sT3}>40,000</p>
+                            </div>
+                            <div className={styles.hash}>
+                                <span className={styles.hashT}>#항산화안티에이징앰플</span>
+                                <span className={styles.hashT}>#안티에이징앰플</span>
+                            </div>
+                            <div className={styles.star}>
+                                <span className={styles.starIcon}><FaStar /></span>
+                                <span className={styles.score}>4.9 (56,096)</span>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <div className={styles.suggestItem}>
-                            <img src="/images/suggestImg03.png" alt="" />
-                            <div className={styles.sText}>
-                                <div className={styles.sText1}>
-                                    <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 앰플 [대용량] x 2개 패키지</span>
-                                </div>
-                                <div className={styles.sText2}>
-                                    <p className={styles.sT1}>76,700</p>
-                                    <p className={styles.sT2}>35%</p>
-                                    <p className={styles.sT3}>118,000</p>
-                                </div>
-                                <div className={styles.hash}>
-                                    <span className={styles.hashT}>항산화안티에이징앰플</span>
-                                    <span className={styles.hashT}>안티에이징앰플</span>
-                                </div>
-                                <div className={styles.star}>
-                                    <span className={styles.starIcon}><FaStar /></span>
-                                    <span className={styles.score}>4.9 (56,096)</span>
-                                </div>
+                </div>
+                <div>
+                    <div className={styles.suggestItem}>
+                        <img src="/images/suggestImg03.png" alt="" />
+                        <div className={styles.sText}>
+                            <div className={styles.sText1}>
+                                <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 앰플 [대용량] x 2개 패키지</span>
+                            </div>
+                            <div className={styles.sText2}>
+                                <p className={styles.sT1}>76,700</p>
+                                <p className={styles.sT2}>35%</p>
+                                <p className={styles.sT3}>118,000</p>
+                            </div>
+                            <div className={styles.hash}>
+                                <span className={styles.hashT}>항산화안티에이징앰플</span>
+                                <span className={styles.hashT}>안티에이징앰플</span>
+                            </div>
+                            <div className={styles.star}>
+                                <span className={styles.starIcon}><FaStar /></span>
+                                <span className={styles.score}>4.9 (56,096)</span>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <div className={styles.suggestItem}>
-                            <img src="/images/suggestImg04.jpg" alt="" />
-                            <div className={styles.sText}>
-                                <div className={styles.sText1}>
-                                    <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 트리트먼트 에센스</span>
-                                </div>
-                                <div className={styles.sText2}>
-                                    <p className={styles.sT1}>31,500</p>
-                                    <p className={styles.sT2}>30%</p>
-                                    <p className={styles.sT3}>45,000</p>
-                                </div>
-                                <div className={styles.hash}>
-                                    <span className={styles.hashT}>항산화워터에센스</span>
-                                    <span className={styles.hashT}>워터에센스</span>
-                                </div>
-                                <div className={styles.star}>
-                                    <span className={styles.starIcon}><FaStar /></span>
-                                    <span className={styles.score}>4.9 (19,873)</span>
-                                </div>
+                </div>
+                <div>
+                    <div className={styles.suggestItem}>
+                        <img src="/images/suggestImg04.jpg" alt="" />
+                        <div className={styles.sText}>
+                            <div className={styles.sText1}>
+                                <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 트리트먼트 에센스</span>
+                            </div>
+                            <div className={styles.sText2}>
+                                <p className={styles.sT1}>31,500</p>
+                                <p className={styles.sT2}>30%</p>
+                                <p className={styles.sT3}>45,000</p>
+                            </div>
+                            <div className={styles.hash}>
+                                <span className={styles.hashT}>항산화워터에센스</span>
+                                <span className={styles.hashT}>워터에센스</span>
+                            </div>
+                            <div className={styles.star}>
+                                <span className={styles.starIcon}><FaStar /></span>
+                                <span className={styles.score}>4.9 (19,873)</span>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <div className={styles.suggestItem}>
-                            <img src="/images/suggestImg05.jpg" alt="" />
-                            <div className={styles.sText}>
-                                <div className={styles.sText1}>
-                                    <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 스킨케어 세트</span>
-                                </div>
-                                <div className={styles.sText2}>
-                                    <p className={styles.sT1}>37,800</p>
-                                    <p className={styles.sT2}>30%</p>
-                                    <p className={styles.sT3}>54,000</p>
-                                </div>
-                                <div className={styles.hash}>
-                                    <span className={styles.hashT}>#블랙티스킨로션세트</span>
-                                    <span className={styles.hashT}>#스킨로션세트</span>
-                                </div>
-                                <div className={styles.star}>
-                                    <span className={styles.starIcon}><FaStar /></span>
-                                    <span className={styles.score}>4.9 (18,012)</span>
-                                </div>
+                </div>
+                <div>
+                    <div className={styles.suggestItem}>
+                        <img src="/images/suggestImg05.jpg" alt="" />
+                        <div className={styles.sText}>
+                            <div className={styles.sText1}>
+                                <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 스킨케어 세트</span>
+                            </div>
+                            <div className={styles.sText2}>
+                                <p className={styles.sT1}>37,800</p>
+                                <p className={styles.sT2}>30%</p>
+                                <p className={styles.sT3}>54,000</p>
+                            </div>
+                            <div className={styles.hash}>
+                                <span className={styles.hashT}>#블랙티스킨로션세트</span>
+                                <span className={styles.hashT}>#스킨로션세트</span>
+                            </div>
+                            <div className={styles.star}>
+                                <span className={styles.starIcon}><FaStar /></span>
+                                <span className={styles.score}>4.9 (18,012)</span>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <div className={styles.suggestItem}>
-                            <img src="/images/suggestImg06.jpg" alt="" />
-                            <div className={styles.sText}>
-                                <div className={styles.sText1}>
-                                    <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 크림</span>
-                                </div>
-                                <div className={styles.sText2}>
-                                    <p className={styles.sT1}>25,200</p>
-                                    <p className={styles.sT2}>30%</p>
-                                    <p className={styles.sT3}>36,000</p>
-                                </div>
-                                <div className={styles.hash}>
-                                    <span className={styles.hashT}>안티에이징크림</span>
-                                    <span className={styles.hashT}>탄력</span>
-                                </div>
-                                <div className={styles.star}>
-                                    <span className={styles.starIcon}><FaStar /></span>
-                                    <span className={styles.score}>4.8 (8,617)</span>
-                                </div>
+                </div>
+                <div>
+                    <div className={styles.suggestItem}>
+                        <img src="/images/suggestImg06.jpg" alt="" />
+                        <div className={styles.sText}>
+                            <div className={styles.sText1}>
+                                <span><span className={styles.bold}>BEST</span>블랙티 유스 인핸싱 크림</span>
+                            </div>
+                            <div className={styles.sText2}>
+                                <p className={styles.sT1}>25,200</p>
+                                <p className={styles.sT2}>30%</p>
+                                <p className={styles.sT3}>36,000</p>
+                            </div>
+                            <div className={styles.hash}>
+                                <span className={styles.hashT}>안티에이징크림</span>
+                                <span className={styles.hashT}>탄력</span>
+                            </div>
+                            <div className={styles.star}>
+                                <span className={styles.starIcon}><FaStar /></span>
+                                <span className={styles.score}>4.8 (8,617)</span>
                             </div>
                         </div>
                     </div>
-                    <div>
+                </div>
+                <div>
                     <div className={styles.suggestItem}>
                         <img src="/images/suggestImg07.png" alt="" />
                         <div className={styles.sText}>
@@ -203,8 +184,8 @@ function SuggestItem() {
                             </div>
                         </div>
                     </div>
-                    </div>
-                    <div>
+                </div>
+                <div>
                     <div className={styles.suggestItem}>
                         <img src="/images/suggestImg08.png" alt="" />
                         <div className={styles.sText}>
@@ -226,8 +207,8 @@ function SuggestItem() {
                             </div>
                         </div>
                     </div>
-                    </div>
-                    <div>
+                </div>
+                <div>
                     <div className={styles.suggestItem}>
                         <img src="/images/suggestImg09.jpg" alt="" />
                         <div className={styles.sText}>
@@ -249,8 +230,8 @@ function SuggestItem() {
                             </div>
                         </div>
                     </div>
-                    </div>
-                    <div>
+                </div>
+                <div>
                     <div className={styles.suggestItem}>
                         <img src="/images/suggestImg10.jpg" alt="" />
                         <div className={styles.sText}>
@@ -272,9 +253,8 @@ function SuggestItem() {
                             </div>
                         </div>
                     </div>
-                    </div>
-                </Slider>
-            </div>
+                </div>
+            </Slider>
         </div>
     );
 }
